d65425d806ef9ad426cd9e2aa10cb2b1b02ba80b
cpschk <cpschk.cl@gmail.com>
2024-01-26 18:58:08 -0300

    Se agrego logica de calculo imc, tmb

--- screens/ImcTmbScreen.tsx
+++ screens/ImcTmbScreen.tsx
@@ -1,86 +1,186 @@
-// ProfileScreen.tsx
-import React, { useState } from 'react';
-// import { View, Text } from 'react-native';
-import { View, Text, TextInput, Button, ScrollView  } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { View, Text, TextInput, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
 import { ImcTmbStyles } from '../styles/ImcTmbScreenStyle';
 
-
 const ImcTmbCalculate = () => {
-  const [estatura, setEstatura] = useState('');
-  const [peso, setPeso] = useState('');
-  const [edad, setEdad] = useState('');
-  const [genero, setGenero] = useState('masculino');
+  const [estatura, setEstatura] = useState<number>(0);
+  const [peso, setPeso] = useState<number>(0);
+  const [edad, setEdad] = useState<string>('');
+  const [rangoIMC, setrangoIMC] = useState<string>('');
+  const [genero, setGenero] = useState<string>('masculino');
+  const [nivelActividad, setNivelActividad] = useState<string>('sedentario');
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [imc, setBmi] = useState<number | null>(null);
+  const [tmbResult, setTmbResult] = useState<number | null>(null);
+  const [caloriesWithActivity, setCaloriesWithActivity] = useState<string | null>(null);
 
-  const [isVisible, setIsVisible] = useState(false);
+  const validarCampo = (campo: string, mensaje: string) => {
+    if (!campo) {
+      alert(mensaje);
+      return false;
+    }
+    return true;
+  };
 
-  const handleEnviar = () => {
-    // Aquí puedes realizar acciones con los datos del formulario
-    console.log('Estatura:', estatura);
-    console.log('Peso:', peso);
-    console.log('Edad:', edad);
-    console.log('Género:', genero);
+  const calcularTmb = (genero: string, peso: number, estatura: number, edad: number): void => {
+    let calculatedTmb: number;
+    if (genero.toLowerCase() === 'masculino') {
+      calculatedTmb = 88.362 + 13.397 * peso + 4.799 * estatura - 5.677 * edad;
+    } else if (genero.toLowerCase() === 'femenino') {
+      calculatedTmb = 447.593 + 9.247 * peso + 3.098 * estatura - 4.330 * edad;
+    }
+    setTmbResult(calculatedTmb);
+    
+  };
+
+  const calculateCaloriesWithActivity = useCallback(() => {
+    const activityFactors = {
+      'sedentario': 1.2,
+      'ligero': 1.375,
+      'moderado': 1.55,
+      'intenso': 1.725,
+    };
+
+    if (activityFactors.hasOwnProperty(nivelActividad)) {
+      const activityFactor = activityFactors[nivelActividad];
+      if (tmbResult !== null && tmbResult !== undefined) {
+        const caloriesWithActivityValue = (tmbResult * activityFactor).toFixed(0);
+        setCaloriesWithActivity(caloriesWithActivityValue);
+      } else {
+        console.error('El resultado TMB no está definido.');
+      }
+    } else {
+      console.error('Nivel de actividad física no válido.');
+    }
+  }, [tmbResult, nivelActividad, setCaloriesWithActivity]);
+
+  const determinarRangoIMC = (imc: number): string => {
+    if (imc < 18.5) {
+      return 'Bajo peso';
+    } else if (imc >= 18.5 && imc < 24.9) {
+      return 'Peso normal';
+    } else if (imc >= 25 && imc < 29.9) {
+      return 'Sobrepeso';
+    } else if (imc >= 30 && imc < 34.9) {
+      return 'Obesidad (Grado I)';
+    } else if (imc >= 35 && imc < 39.9) {
+      return 'Obesidad (Grado II)';
+    } else {
+      return 'Obesidad severa (Grado III)';
+    }
+  };
+
+  const handleResult = () => {
+    if (!validarCampo(estatura.toString(), 'Por favor, ingrese la estatura.')) return;
+    if (!validarCampo(peso.toString(), 'Por favor, ingrese el peso.')) return;
+    if (!validarCampo(edad, 'Por favor, ingrese la edad.')) return;
+
+    const bmi = peso / ((estatura / 100) * (estatura / 100));
+
+    setBmi(bmi);
+    setrangoIMC(determinarRangoIMC(bmi));
+    calcularTmb(genero, peso, estatura, Number(edad));
+    calculateCaloriesWithActivity();
+
+    // console.log('Estatura:', estatura);
+    // console.log('Peso:', peso);
+    // console.log('Edad:', edad);
+    // console.log('Género:', genero);
     setIsVisible(!isVisible);
   };
 
   return (
-    // <View style={ImcTmbStyles.container}>
     <ScrollView contentContainerStyle={ImcTmbStyles.container}>
-      <Text style={ImcTmbStyles.label}>Estatura (cm):</Text>
-      <TextInput
-        style={ImcTmbStyles.input}
-        keyboardType="numeric"
-        placeholder="Ingrese la estatura"
-        value={estatura}
-        onChangeText={(text) => setEstatura(text)}
-      />
+      <View style={ImcTmbStyles.dataContainer}>
+        <View style={ImcTmbStyles.data}>
+          <Text style={ImcTmbStyles.label}>Estatura (cm):</Text>
+          <TextInput
+            style={ImcTmbStyles.input}
+            keyboardType="numeric"
+            placeholder="Ingrese la estatura"
+            value={estatura.toString()}
+            onChangeText={(text) => setEstatura(parseFloat(text))}
+          />
 
-      <Text style={ImcTmbStyles.label}>Peso (kg):</Text>
-      <TextInput
-        style={ImcTmbStyles.input}
-        keyboardType="numeric"
-        placeholder="Ingrese el peso"
-        value={peso}
-        onChangeText={(text) => setPeso(text)}
-      />
+          <Text style={ImcTmbStyles.label}>Peso (kg):</Text>
+          <TextInput
+            style={ImcTmbStyles.input}
+            keyboardType="numeric"
+            placeholder="Ingrese el peso"
+            value={peso.toString()}
+            onChangeText={(text) => setPeso(parseFloat(text))}
+          />
 
-      <Text style={ImcTmbStyles.label}>Edad:</Text>
-      <TextInput
-        style={ImcTmbStyles.input}
-        keyboardType="numeric"
-        placeholder="Ingrese la edad"
-        value={edad}
-        onChangeText={(text) => setEdad(text)}
-      />
+          <Text style={ImcTmbStyles.label}>Edad:</Text>
+          <TextInput
+            style={ImcTmbStyles.input}
+            keyboardType="numeric"
+            placeholder="Ingrese la edad"
+            value={edad}
+            onChangeText={(text) => setEdad(text)}
+          />
 
-      <Text style={ImcTmbStyles.label}>Género:</Text>
-      <Picker
-        selectedValue={genero}
-        onValueChange={(itemValue) => setGenero(itemValue)}
-        style={ImcTmbStyles.picker}
-      >
-        <Picker.Item label="Masculino" value="masculino" />
-        <Picker.Item label="Femenino" value="femenino" />
-      </Picker>
+          <Text style={ImcTmbStyles.label}>Género:</Text>
+          <Picker
+            selectedValue={genero}
+            onValueChange={(itemValue) => setGenero(itemValue)}
+            style={ImcTmbStyles.picker}
+          >
+            <Picker.Item label="Masculino" value="masculino" />
+            <Picker.Item label="Femenino" value="femenino" />
+          </Picker>
 
-      <Button title={isVisible ? 'Ocultar Resultado' : 'Mostrar Resultado'} onPress={handleEnviar} />
-
+          <Text style={ImcTmbStyles.label}>Nivel de actividad física:</Text>
+          <Picker
+            selectedValue={nivelActividad}
+            onValueChange={(itemValue) => setNivelActividad(itemValue)}
+            style={ImcTmbStyles.picker}
+          >
+            <Picker.Item label="Sedentario" value="sedentario" />
+            <Picker.Item label="Ligero" value="ligero" />
+            <Picker.Item label="Moderado" value="moderado" />
+            <Picker.Item label="Intenso" value="intenso" />
+          </Picker>
+        </View>
+        <View>
+          <Image source={require('../assets/paper-fruits.png')} style={ImcTmbStyles.image} />
+        </View>
+      </View>
+      <View style={ImcTmbStyles.buttonContainer}>
+        <TouchableOpacity style={ImcTmbStyles.button} onPress={handleResult}>
+          <Text style={ImcTmbStyles.buttonText}>
+            {isVisible ? 'Ocultar Resultado' : 'Mostrar Resultado'}
+          </Text>
+        </TouchableOpacity>
+      </View>
 
       {isVisible && (
         <View style={ImcTmbStyles.hiddenView}>
-        <Text> Aqui se vera el 
-          resultado asdasdasdasdas
-          dasdasdkjsadh ckhsds jkd dfdjfsalk
-          hfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjz
-          hfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfask
-          hfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfask
-          hfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskhfclwenlfhdfjcjzjhcrlrehñuñeuyuaeuhfaskjhcrlrehñuñeuyuaeuhfaskjhcrlrehñuñeuyuaeuhfask
-          jhafkjdsañtñehsf-kjsdkfjahsgkjhaekhekñhfsakfhksajhgksajhgwehwhkewjhgkjsdahjkdvdjsghEHEGSDHFÑUUTUYWHFJSAHCUEWRYBW4IUWICJHASECBAÑEBÑABVAUVBAÑKEJBFWAHBBABFSÑIHBTtbsñngbñsbñfhgk
-          jdsfhgkdlshglkdsde IMC</Text>
-      </View>
+          <Text>
+            Aqui se vera el resultado asdasdasdasdas{'\n'}
+          </Text>
+
+          <View style={ImcTmbStyles.imcTmbView}>
+            <Text style={ImcTmbStyles.imctmb}>
+              IMC{'\n'}
+              {imc?.toFixed(2)}
+            </Text>
+
+            <Text style={ImcTmbStyles.imctmb}>
+              TMB{'\n'}
+              {tmbResult.toFixed(0)}
+            </Text>
+          </View>
+
+          <Text>
+            El IMC se clasifica como:{'\n'} {rangoIMC}{'\n'}
+            Con actividad física{'\n'}
+            {caloriesWithActivity} kcal
+          </Text>
+        </View>
       )}
-    {/* </View> */}
     </ScrollView>
   );
 };
--- styles/ImcTmbScreenStyle.tsx
+++ styles/ImcTmbScreenStyle.tsx
@@ -2,9 +2,18 @@ import { StyleSheet } from 'react-native';
 
 export const ImcTmbStyles = StyleSheet.create({
   container: {
-    // flex:1,
     flexGrow: 1,
     padding: 16,
+  },
+  dataContainer: {
+    flexDirection: 'row',
+  },
+  data: {
+    padding: 10,
+  },
+  image: {
+    width: 170,
+    height: 250,
   },
   label: {
     fontSize: 16,
@@ -22,10 +31,42 @@ export const ImcTmbStyles = StyleSheet.create({
     height: 40,
     marginBottom: 12,
   },
+  buttonContainer: {
+    alignItems: 'center',
+  },
+  button: {
+    width: 160,
+    height: 40,
+    marginTop: 10,
+    backgroundColor: 'blue',
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  },
   hiddenView: {
+    alignItems: 'center',
     marginTop: 20,
     padding: 10,
     backgroundColor: '#e0e0e0',
     borderRadius: 8,
   },
+  imcTmbView: {
+    width: 250,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  imctmb: {
+    textAlign: 'center',
+    fontSize: 20,
+    padding: 10,
+    width: 100,
+    height: 100,
+    borderWidth: 1,
+  }
 });
